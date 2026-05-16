5851677495e9e92cfd1ba521ad1eab52c43cedb7
rudrapratap601 <rudrapratapsarma549@gmail.com>
2026-05-16 17:13:28 +0530

    connected with database server

--- backend/db.py
+++ backend/db.py
@@ -1,26 +1,13 @@
 from sqlalchemy import create_engine
-from urllib.parse import quote_plus
 import streamlit as st
 
 
 def get_engine():
 
-    db_user = st.secrets["DB_USER"]
-    db_password = st.secrets["DB_PASSWORD"]
-    db_host = st.secrets["DB_HOST"]
-    db_port = st.secrets["DB_PORT"]
-    db_name = st.secrets["DB_NAME"]
-
-    db_password = quote_plus(db_password)
-
-    connection_string = (
-        f"postgresql+psycopg2://"
-        f"{db_user}:{db_password}"
-        f"@{db_host}:{db_port}/{db_name}"
-    )
+    database_url = st.secrets["DATABASE_URL"]
 
     engine = create_engine(
-        connection_string
+        database_url
     )
 
     return engine
