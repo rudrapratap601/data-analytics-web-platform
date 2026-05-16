41004d2629b491148f28d7cadf75a8c3aaf2200f
rudrapratap601 <rudrapratapsarma549@gmail.com>
2026-05-16 18:52:40 +0530

    updated database

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,31 @@
 from sqlalchemy import create_engine
+from urllib.parse import quote_plus
 import streamlit as st
 
 
 def get_engine():
 
-    database_url = st.secrets["DATABASE_URL"]
+    db_user = st.secrets["DB_USER"]
+
+    db_password = quote_plus(
+        st.secrets["DB_PASSWORD"]
+    )
+
+    db_host = st.secrets["DB_HOST"]
+
+    db_port = st.secrets["DB_PORT"]
+
+    db_name = st.secrets["DB_NAME"]
+
+    connection_string = (
+        f"postgresql+psycopg2://"
+        f"{db_user}:{db_password}"
+        f"@{db_host}:{db_port}/{db_name}"
+        f"?sslmode=require"
+    )
 
     engine = create_engine(
-        database_url
+        connection_string
     )
 
     return engine
